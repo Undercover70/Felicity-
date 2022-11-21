ef515c8adbbfd2f4efad1e131ed2f1f73a9dcd61
Undercover70 <118295842+Undercover70@users.noreply.github.com>
2022-11-21 14:53:59 +0100

    Update Core.js

--- Core.js
+++ Core.js
@@ -4978,7 +4978,7 @@ default:
     if(isCmd){
         if (isBan) return reply(mess.banned)	 			
         if (isBanChat) return reply(mess.bangc)
-        reply (`No such command programmed *${pushname}* senpai! Type *${prefix}help* to get my full command list!`)
+        reply (`No such command programmed *${pushname}* pls Type *${prefix}help* to get my full command list!`)
 
     }	 			
 
